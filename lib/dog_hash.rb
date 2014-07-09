e51159e8b21b7e83bb425f35520cb50328f11727
Alex McRitchie <amcritchie@gmail.com>
2014-07-09 09:52:39 -0600

    two to go.

--- lib/dog_hash.rb
+++ lib/dog_hash.rb
@@ -1,7 +1,8 @@
 DOG = {
+    "harleigh" => {preferences: [name: "Harleigh", favorite_toy: "frog"]},
+    "trixie" => {preferences: [name: "Harleigh", favorite_food: "steak"]},
     cattle_dog: {
         preferences: [name: "Harleigh"],
         names: ['Harleigh', 'brown']
     }
 }
-
